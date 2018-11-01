293232ecbaa6080e7fa2add66ba7101ce59202dc
GustavoAntunesRocha <gustavo.antunes.rocha@outlook.com>
2018-11-01 16:36:58 -0300

    Diversas modificacoes para atender os requisitos

--- app.js
+++ app.js
@@ -1,37 +1,45 @@
-var fs = require('fs')
+var fs = require('fs');
+var http = require('http');
+var io = require('socket.io');
+var engine = require('engine.io');
+var NetSocket = require("net").Socket;
+var IrcSocket = require("irc-socket");
+var netSocket = new NetSocket();
+var client = IrcSocket({
+    socket: netSocket,
 
-, http = require('http')
-
-, socketio = require('socket.io'), irc = require('irc');
-
-var servidor = 'irc.freenode.net';
-var nick = 'teste2';
-var canal = '#adsiweb';
-var dentro_do_canal = false;
-
-var client = new irc.Client(servidor,
-    nick, { channels: [canal], }
-);
-
-var server = http.createServer(function(req, res) {
+    port: 6667,
+    server: "irc.freenode.net",
+    nicknames: ["freddy", "freddy_"],
+    username: "freddy",
+    realname: "Freddy",
+});
+var serverHttp = http.createServer(function(req, res) {
 
     res.writeHead(200, { 'Content-type': 'text/html' });
 
     res.end(fs.readFileSync(__dirname + '/index.html'));
 
-}).listen(8080, function() {
+}).listen(3000, function() {
 
-    console.log('Listening at: http://localhost:8080');
+    console.log('Listening at: http://localhost:3000');
 
 });
+//var server = engine.attach(serverHttp);
 
-socketio.listen(server).on('connection', function(socket) {
-
-    socket.on('message', function(msg) {
-
-        console.log('Message Received: ', msg);
-
-        socket.broadcast.emit('message', msg);
+io.listen(serverHttp).on('connection', function(socket) {
+    console.log("Socket conectou");
+    client.connect();
+    client.raw("JOIN #biscoitos");
+    socket.on('message', function(data) {
+        console.log(data);
+        client.raw(data);
     });
+    client.on('data', function(message) {
+        console.log(message);
+        socket.send(message);
+    });
+    socket.on('close', function() {
 
+    });
 });
